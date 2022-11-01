fe1596481be4318a9d8c1e932bea83d89ba3f206
vagif-guliyev <vaguliyev@gmail.com>
2022-11-02 03:44:43 +0400

    Add separate pagees for each post

--- Blog Page/app.js
+++ Blog Page/app.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-var _ = require("lodash");
+const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -51,16 +51,32 @@ app.post('/compose', function(req, res) {
   res.redirect("/");
 });
 
+// app.get('/posts/:postName', function(req, res) {
+//   console.log(req.params.postName);
+//   posts.forEach(function(post) {
+//     if(_.lowerCase(post.title) === _.lowerCase(req.params.postName)) {
+//       console.log("Match found");
+//     } else {
+//       console.log("No match found");
+//     }
+//   });
+// });
+
 app.get('/posts/:postName', function(req, res) {
-  console.log(req.params.postName);
   posts.forEach(function(post) {
-    if(_.lowerCase(post.title) === _.lowerCase(req.params.postName)) {
-      console.log("Match found");
-    } else {
-      console.log("No match found");
-    }
-  });
+        if(_.lowerCase(post.title) === _.lowerCase(req.params.postName)) {
+            console.log("Match found");
+            res.render('post', {
+              title: post.title, 
+              content: post.content
+            });
+        } else {
+          console.log("No match found");
+        }
+      });
 });
+
+
 
 
 
